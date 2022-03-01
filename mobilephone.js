9e596cbe16846065ea38c5893fae01eaf7754baf
unknown <you@example.com>
2022-03-01 18:10:31 +0600

    move to js

--- mobilephone.js
+++ mobilephone.js
@@ -8,5 +8,23 @@ const searchMobile = () => {
     // console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data));
+        .then(data => displaySearchResult(data.data));
+}
+
+const displaySearchResult = data => {
+    const searchResult = document.getElementById('search-result');
+    data.forEach(phone => {
+        console.log(phone);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card h-100">
+                <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <h2 class="card-text">${phone.brand}</h2>
+                    <button id="button-details" type="button" class="btn btn-primary">Explore</button>
+                </div>
+            </div>`;
+        searchResult.appendChild(div)
+    })
 }
