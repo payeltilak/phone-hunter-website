63e87758417307eaf62c3009e55bea2020fcc29f
unknown <you@example.com>
2022-03-02 22:28:59 +0600

    added background

--- mobilephone.js
+++ mobilephone.js
@@ -18,7 +18,7 @@ const displayPhoneDetails = (phones) => {
     error.innerHTML = '';
     if (phones.length == 0) {
         const div = document.createElement('div')
-        div.innerHTML = `<p class="text-center">no result found</p>`;
+        div.innerHTML = `<p class="text-center text-danger fw-bold">No result found</p>`;
         error.appendChild(div);
 
     }
@@ -44,38 +44,44 @@ const phoneIDeatil = (phoneId) => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
-        .then(data => dispalyShowDetails(data.data))
+        .then(data => dispalyShowDetails(data?.data))
+
 }
 const dispalyShowDetails = phone => {
-    console.log(phone);
+    console.log(phone.mainFeatures.chipSet);
     const phoneInfo = document.getElementById('phone-info');
     phoneInfo.innerHTML = '';
     const div = document.createElement('div');
     div.classList.add('card');
 
     div.innerHTML = `
-    <div class="mb-5 mx-auto mt-5 d-flex"> 
-     <img src="${phone.image}" class="card-img-top w-25 h-25 pt-5" alt="...">
+    <div class="mb-5 mx-auto mt-5 d-flex justify-content-center "> 
+     <div class="d-flex justify-content-center align-items-center">
+     <img  src="${phone.image}" class="card-img-top p-5 h-75" alt="...">
+     </div>
    <div class="card-body mx-auto">
-       <h2 class="card-title">${phone.brand}</h2>
-       <h3><span>Release Date:</span>${phone.releaseDate ? phone.releaseDate : 'Not found'}</h3>
-       <h3><span>slug:</span>${phone.slug}</h3>
-       <h3><span>Name:</span>${phone.name}</h3>
-       
-     <h2 class="card-title">Features</h2>
-       <p><span> DisplaySize: </span>${phone.mainFeatures.displaySize}</p>
-       <p><span>Storage: </span>${phone.mainFeatures.storage}</p>
-       <p><span>Chipset: </span>${phone.mainFeatures.chipSet}</p>
-       <p><span>Memory: </span>${phone.mainFeatures.memory}</p>
-       <p><span>Sensors: </span>${phone.mainFeatures.sensors}</p>
+       <h2 class="card-title fw-bold">${phone.brand}</h2>
+       <h3><span class="text-primary">Release Date : </span>${phone.releaseDate ? phone.releaseDate : 'Not found'}</h3>
+      <h3><span class="text-primary">Name:</span>${phone.name}</h3>
+      
+      
         
-       <h2 class="card-title">Others</h2>
-       <p><span> WLAN </span>${phone.others ? phone.others.WLAN : 'Not Found'}</p>
-       <p><span> Bluetooth: </span>${phone.others ? phone.others.Bluetooth : 'Not Found'}</p>
+       <h2 class="">Others</h2>
+
+       <p><span class="text-primary"> WLAN:  </span>${phone.others ? phone.others.WLAN : 'Not Found'}</p>
+       <p><span > Bluetooth: </span>${phone.others ? phone.others.Bluetooth : 'Not Found'}</p>
         <p><span> GPS:  </span>${phone.others ? phone.others.GPS : 'Not Found'}</p>
        <p><span> NFC:</span>${phone.others ? phone.others.NFC : 'Not Found'}</p>
        <p><span> Radio: </span>${phone.others ? phone.others.Radio : 'Not Found'}</p>
        <p><span> USB: </span>${phone.others ? phone.others.USB : 'Not Found'}</p>
+
+       <h2>Features</h2>
+      <p><span  class="text-primary fw-bold> DisplaySize: </span>${phone.mainFeatures?.displaySize}</p>
+      <p><span  class=" text-primary fw-bold>Storage: </span>${phone.mainFeatures?.storage}</p>
+      <p><span class="class-primary fw-bold>ChipSet:</span>${phone.mainFeatures?.chipSet}"</p>
+      <p><span  class=" text-primary fw-bold>Memory: </span>${phone.mainFeatures?.memory}</p>
+     <p><span class="class-primary fw-bold>Sensors:</span>${phone.mainFeatures?.sensors}"</p>
+     <p><span  class=" text-primary fw-bold>Storage: </span>${phone.mainFeatures.chipSet}</p>
 
    </div >
  </div> `;
